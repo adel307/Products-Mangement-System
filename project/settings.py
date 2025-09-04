dae6e1290d9d8f1927da2eb8002de94a726679b4
adel307 <adel.307.ahmed@gmail.com>
2025-09-04 20:32:05 +0300

    add jazmin library

--- project/settings.py
+++ project/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Custom Django Admin
+    'jazzmin',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
